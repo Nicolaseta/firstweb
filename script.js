5a1866776e63fee7a2e0078c0601b9826a108bc9
Nicolaseta <142957667+Nicolaseta@users.noreply.github.com>
2023-08-22 21:37:58 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+    // Aquí iría la lógica para validar el inicio de sesión
+    // Redirigir a la página de perfil si es exitoso
+    window.location.href = "profile.html";
+}
+
+function register() {
+    // Aquí iría la lógica para registrar al usuario
+    // Redirigir a la página de inicio de sesión después del registro
+    window.location.href = "login.html";
+}
+
+function logout() {
+    // Aquí iría la lógica para cerrar sesión
+    // Redirigir a la página de inicio de sesión después del cierre de sesión
+    window.location.href = "login.html";
+}
